fcda11975bf4545640260e96c361aba0c96ba726
Samuel Livingston <164835921+JinxSeven@users.noreply.github.com>
2026-03-09 21:55:34 +0530

    Entities and Enums
    User and UserRole

---
+++ apps/server/api/Modules/ChMS.Modules.Auth/Core/Entities/User.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using ChMS.Modules.Auth.Core.Enums;
+
+namespace ChMS.Modules.Auth.Core.Entities
+{
+    internal class User
+    {
+        public Guid Id { get; set; }
+        public required string Username { get; set; }
+        public required string Email { get; set; }
+        public required string PasswordHash { get; set; }
+        public UserRole Role { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ apps/server/api/Modules/ChMS.Modules.Auth/Core/Enums/UserRole.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChMS.Modules.Auth.Core.Enums
+{
+    internal enum UserRole
+    {
+        SuperAdmin = 1,
+        Admin = 2,
+        User = 3,
+    }
+}
