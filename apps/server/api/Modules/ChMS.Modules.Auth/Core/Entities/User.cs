022a558b483dfb03f4d317ddf383c6b3d42b5b71
Samuel Livingston <164835921+JinxSeven@users.noreply.github.com>
2026-03-22 19:01:54 +0530

    Update User.cs
    Improved User entity

--- apps/server/api/Modules/ChMS.Modules.Auth/Core/Entities/User.cs
+++ apps/server/api/Modules/ChMS.Modules.Auth/Core/Entities/User.cs
@@ -1,17 +1,24 @@
 using System.ComponentModel.DataAnnotations;
 using ChMS.Modules.Auth.Core.Enums;
+using Microsoft.EntityFrameworkCore;
 
 namespace ChMS.Modules.Auth.Core.Entities
 {
+    [Index(nameof(Email), IsUnique = true)]
     public class User
     {
         public Guid Id { get; set; }
+
+        [MaxLength(100)]
         public required string Username { get; set; }
+
+        [MaxLength(150)]
+        [EmailAddress]
         public required string Email { get; set; }
         public required string PasswordHash { get; set; }
         public UserRole Role { get; set; }
         public bool IsActive { get; set; }
         public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
     }
 }
