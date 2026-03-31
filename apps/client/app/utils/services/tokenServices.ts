4918d6b07fb21020f9350e054f4f03994a7b6304
Samuel Livingston <164835921+JinxSeven@users.noreply.github.com>
2026-03-31 09:21:50 +0530

    Added getAccessToken

--- apps/client/app/utils/services/tokenServices.ts
+++ apps/client/app/utils/services/tokenServices.ts
@@ -1,6 +1,17 @@
 const ACCESS_TOKEN_NAME = "access-token-chms";
 
-export const setAuthAccessToken = (token: string, expiresOn: string) => {
+export const setAccessToken = (token: string, expiresOn: string) => {
   const maxAge = Math.floor(new Date(expiresOn).getTime() - Date.now() / 1000);
   document.cookie = `${ACCESS_TOKEN_NAME}=${token}; path=/; max-age=${maxAge}; SameSite=Strict`;
+};
+
+export const getAccessToken = () => {
+  const tokenName = ACCESS_TOKEN_NAME + "=";
+  const decoded = decodeURIComponent(document.cookie);
+  const arr = decoded.split("; ");
+  let res;
+  arr.forEach((val) => {
+    if (val.indexOf(tokenName) === 0) res = val.substring(tokenName.length);
+  });
+  return res;
 };
