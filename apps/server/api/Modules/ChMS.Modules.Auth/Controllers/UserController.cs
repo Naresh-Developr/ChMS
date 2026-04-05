35ba8e836944e59d67d4de06b04bb124942cfc2f
Samuel Livingston <164835921+JinxSeven@users.noreply.github.com>
2026-04-05 22:29:36 +0530

    Authorize Controller

--- apps/server/api/Modules/ChMS.Modules.Auth/Controllers/UserController.cs
+++ apps/server/api/Modules/ChMS.Modules.Auth/Controllers/UserController.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using System.Text;
 using ChMS.Modules.Auth.Application.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ChMS.Modules.Auth.Controllers
 {
+    [Authorize]
+    [ApiController]
     [Route("api/user")]
     public class UserController(UserService userService) : ControllerBase
     {
         private readonly UserService _userService = userService;
 
+        [Authorize(Roles = "Admin, SuperAdmin")]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetUserById(Guid id)
         {
