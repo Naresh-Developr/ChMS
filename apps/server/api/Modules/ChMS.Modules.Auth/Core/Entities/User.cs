ec4a5307f11deb9dc12947f82af5f7e26303d4d0
Samuel Livingston <164835921+JinxSeven@users.noreply.github.com>
2026-04-12 12:34:11 +0530

    Improve User Table
    Added data annotations to prevent empty string and other invalid data inputs.

--- apps/server/api/Modules/ChMS.Modules.Auth/Core/Entities/User.cs
+++ apps/server/api/Modules/ChMS.Modules.Auth/Core/Entities/User.cs
@@ -10,12 +10,17 @@ namespace ChMS.Modules.Auth.Core.Entities
         public Guid Id { get; set; }
 
         [MaxLength(100)]
+        [Required]
+        [MinLength(3)]
         public required string Name { get; set; }
 
         [MaxLength(150)]
-        [EmailAddress]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        [Required]
         public required string Email { get; set; }
         public required string PasswordHash { get; set; }
+
+        [Required]
         public UserRole Role { get; set; }
         public bool IsActive { get; set; }
         public DateTime CreatedAt { get; set; }
