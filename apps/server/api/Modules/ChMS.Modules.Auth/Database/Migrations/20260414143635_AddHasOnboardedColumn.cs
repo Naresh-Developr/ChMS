1cbf8501b52cee96466fa796de53e234e274301b
Samuel Livingston <164835921+JinxSeven@users.noreply.github.com>
2026-04-14 23:13:18 +0530

    Added User Table Migration

---
+++ apps/server/api/Modules/ChMS.Modules.Auth/Database/Migrations/20260414143635_AddHasOnboardedColumn.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChMS.Modules.Auth.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHasOnboardedColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "HasOnboarded",
+                schema: "auth",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HasOnboarded",
+                schema: "auth",
+                table: "Users");
+        }
+    }
+}
